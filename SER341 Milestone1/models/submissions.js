aba1535e4cae21e2a688249b3b4aa36f0bfc10a1
amberk887 <amber.kusma@quinnipiac.edu>
2023-04-09 20:02:32 -0400

    updated models and routes

--- SER341 Milestone1/models/submissions.js
+++ SER341 Milestone1/models/submissions.js
@@ -1,22 +1,25 @@
 
 const { Double } = require('mongodb');
+const Student = require('../models/student');
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
 var submissionSchema = new Schema({
-	username : { 
-			type : String,
+	student : { 
+			type : Schema.Types.ObjectId,
+			ref: 'Student',
 			required: true,
 			},
-	assignment : {
-			type : String,
-			required : true
-	},
     grade : { 
         type : String,
         required: true,
-        }
+        },
+	assignment: {
+		type: Schema.Types.ObjectId,
+		ref:'Assignment'
+		}
 });
 
 var Submissions = mongoose.model('Submissions', submissionSchema);
